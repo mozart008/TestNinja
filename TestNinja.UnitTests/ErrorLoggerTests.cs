ee47c0ebe94913671b94a2cef322a9090efaab13
mozart <mozartgranado08@gmail.com>
2026-02-16 15:40:22 +0800

    Added-tests-fizzbuzz

--- TestNinja.UnitTests/ErrorLoggerTests.cs
+++ TestNinja.UnitTests/ErrorLoggerTests.cs
@@ -33,5 +33,19 @@ namespace TestNinja.UnitTests
 
             Assert.That(() => logger.Log(error), Throws.ArgumentNullException);
         }
+
+        [Test]
+        public void Log_ValidError_RaiseErrorLoggedEvent()
+        {
+            var logger = new ErrorLogger();
+
+            var id = Guid.Empty;
+            logger.ErrorLogged += (sender, args) => { id = args; };
+
+            logger.Log("a");
+
+            Assert.That(id, Is.Not.EqualTo(Guid.Empty));
+        }
+
     }
 }
---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,59 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        [TestCase(15)]
+        [TestCase(30)]
+        [TestCase(45)]
+        public void GetOutput_InputIsDivisibleBy3And5_ReturnFizzBuzz(int input)
+        {
+            var result = FizzBuzz.GetOutput(input);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(6)]
+        [TestCase(9)]
+        public void GetOutput_InputIsDivisibleBy3Only_ReturnFizz(int input)
+        {
+            var result = FizzBuzz.GetOutput(input);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+
+        [Test]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(20)]
+        public void GetOutput_InputIsDivisibleBy5Only_ReturnBuzz(int input)
+        {
+            var result = FizzBuzz.GetOutput(input);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(4)]
+        public void GetOutput_InputIsNotDivisibleBy3Or5_ReturnTheSameNumber(int input)
+        {
+            var result = FizzBuzz.GetOutput(input);
+
+            Assert.That(result, Is.EqualTo(input.ToString()));
+        }
+    }
+}
